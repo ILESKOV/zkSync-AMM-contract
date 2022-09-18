b8808d13f78910d511330a97f0a3815553d0f9b3
Ivan Leskov <103837756+ILESKOV@users.noreply.github.com>
2022-09-19 00:20:27 +0200

    deploy script to zkSync

--- deploy/deploy.ts
+++ deploy/deploy.ts
@@ -7,7 +7,7 @@ const { PRIVATE_KEY } = process.env;
 
 // An example of a deploy script that will deploy and call a simple contract.
 export default async function (hre: HardhatRuntimeEnvironment) {
-  console.log(`Running deploy script for the Greeter contract`);
+  console.log(`Running deploy script for the CPAMM contract`);
 
   // Initialize the wallet.
   const provider = new Provider(hre.userConfig.zkSyncDeploy?.zkSyncNetwork);
@@ -15,34 +15,48 @@ export default async function (hre: HardhatRuntimeEnvironment) {
 
   // Create deployer object and load the artifact of the contract you want to deploy.
   const deployer = new Deployer(hre, wallet);
-  const artifact = await deployer.loadArtifact("Greeter");
+  const token0_artifact = await deployer.loadArtifact("token0");
+  const token1_artifact = await deployer.loadArtifact("token1");
+  const CPAMM_artifact = await deployer.loadArtifact("CPAMM");
 
   // Estimate contract deployment fee
-  const greeting = "Hi there!";
-  const deploymentFee = await deployer.estimateDeployFee(artifact, [greeting]);
+  const token0_simulation = "0x739712f6be312dafd8b71a1d33e30c5ed69514e2";
+  const token1_simulation = "0xf8dff94dacd3b136ef097ebaae5f6c064a2dbc42";
+  const deploymentFee = await deployer.estimateDeployFee(CPAMM_artifact, [
+    token0_simulation,
+    token1_simulation,
+  ]);
 
   // Deposit funds to L2
   const depositHandle = await deployer.zkWallet.deposit({
     to: deployer.zkWallet.address,
     token: utils.ETH_ADDRESS,
-    amount: deploymentFee.mul(2),
+    amount: deploymentFee.mul(3),
   });
   // Wait until the deposit is processed on zkSync
   await depositHandle.wait();
 
   // Deploy this contract. The returned object will be of a `Contract` type, similarly to ones in `ethers`.
-  // `greeting` is an argument for contract constructor.
+  // `token0.address` and `token1.address` are an arguments for contract constructor.
   const parsedFee = ethers.utils.formatEther(deploymentFee.toString());
   console.log(`The deployment is estimated to cost ${parsedFee} ETH`);
 
-  const greeterContract = await deployer.deploy(artifact, [greeting]);
+  const token0 = await deployer.deploy(token0_artifact);
+  const token1 = await deployer.deploy(token1_artifact);
+  const CPAMM_contract = await deployer.deploy(CPAMM_artifact, [
+    token0.address,
+    token1.address,
+  ]);
 
   //obtain the Constructor Arguments
   console.log(
-    "constructor args:" + greeterContract.interface.encodeDeploy([greeting])
+    "constructor args:" +
+      CPAMM_contract.interface.encodeDeploy([token0.address, token1.address])
   );
 
   // Show the contract info.
-  const contractAddress = greeterContract.address;
-  console.log(`${artifact.contractName} was deployed to ${contractAddress}`);
+  const contractAddress = CPAMM_contract.address;
+  console.log(
+    `${CPAMM_artifact.contractName} was deployed to ${contractAddress}`
+  );
 }
